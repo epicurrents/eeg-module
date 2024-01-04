2f8af075d86b2f7b6fef0a3247ae4b8a147739e1
sam-19 <26563023+sam-19@users.noreply.github.com>
2024-01-04 09:55:21 +0200

    Yet improve types
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+import {
+    type EegHeaderSignal,
+    type EegModuleSettings,
+    type EegResource,
+    type EegStudyProperties,
+} from './types/index'
+
+export {
+    EegHeaderSignal,
+    EegModuleSettings,
+    EegResource,
+    EegStudyProperties,
+}
